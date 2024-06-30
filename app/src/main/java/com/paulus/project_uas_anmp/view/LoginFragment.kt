c0a2fa43e1acec012fc28b57e2a1b6d645da30a8
Calvin <s160421012@student.ubaya.ac.id>
2024-06-30 11:36:57 +0700

    perbaikan pada loginFragment

--- app/src/main/java/com/paulus/project_uas_anmp/view/LoginFragment.kt
+++ app/src/main/java/com/paulus/project_uas_anmp/view/LoginFragment.kt
@@ -8,42 +8,42 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
-import com.paulus.project_uas_anmp.R
 import com.paulus.project_uas_anmp.databinding.FragmentLoginBinding
-import com.paulus.project_uas_anmp.databinding.FragmentRegistBinding
 import com.paulus.project_uas_anmp.model.User
 import com.paulus.project_uas_anmp.viewmodel.UserViewModel
 
 class LoginFragment : Fragment(), RegistClick, LoginClick {
     private lateinit var viewModel: UserViewModel
     private lateinit var binding: FragmentLoginBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel =
-            ViewModelProvider(this).get(UserViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        // Create a User object and bind it to the layout
+        binding.user = User(username = "", email = "", password = "")
+
+        // Set the click listeners
         binding.loginListener = this
         binding.registListener = this
     }
 
     override fun onLoginClick(v: View, obj: User) {
-        if (obj.password != "" || obj.username != "")
-        {
+        if (obj.username.isNotEmpty() && obj.password.isNotEmpty()) {
             viewModel.login(obj.username, obj.password)
             Toast.makeText(v.context, "Welcome", Toast.LENGTH_LONG).show()
-            val action = LoginFragmentDirections.actionLoginNewsListFragment(v.tag.toString())
+            val action = LoginFragmentDirections.actionLoginNewsListFragment(obj.username)
             Navigation.findNavController(v).navigate(action)
-        }
-        else
-        {
-            Toast.makeText(v.context, "You should fill this blank", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(v.context, "You should fill in all the fields", Toast.LENGTH_SHORT).show()
         }
     }
 
